cd18fcc735ec83da73dd1db659aec9bfbf78b754
darkhost <cryptDecoder@gmail.com>
2021-11-22 00:14:04 +0530

    initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
